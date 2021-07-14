5a13dbe4a155bb3c291361a7cc15f24a21e1ca4c
Malan Himsara <87243006+redx414@users.noreply.github.com>
2021-07-14 09:48:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,9 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("🔥Channel🔥", url="https://t.me/joinchat/dK29OVb4ktxkODhl")],
-        [InlineKeyboardButton(
-            "💢Contact Admin💢", url="https://t.me/@RedX14")]
+        [InlineKeyboardButton("🔥Channel🔥", url="https://t.me/redx414news")],
+        [InlineKeyboardButton("💢Contact Admin💢", url="https://t.me/RedX14")]
     ])
-    welcomed = f"Hello <b>{message.from_user.first_name}</b>\n\n🌼You Can Download Any YT Video Using Our Bot🌼\nSend the URL Of the Video and See the Magic😍"
+    welcomed = f"Hello <b>{message.from_user.first_name}</b>\n\n🌼You Can Download Any YT Video Using Our Bot🌼\n😍Send the URL Of the Video and See the Magic😍"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
