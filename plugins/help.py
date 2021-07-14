aa647ed97916f2c653c7a4f512c966736f67d360
Malan Himsara <87243006+redx414@users.noreply.github.com>
2021-07-14 10:28:37 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,7 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help414"]))
 async def start(client, message):
-    helptxt = f"🚫You Can't Download YT Playlists From Our Bot🚫
-
-🔥Just Download One Video at a Time🔥"
+    helptxt = f"You Can't Download YT Playlists From Our Bot🚫Just Download One Video at a Time🔥"
     await message.reply_text(helptxt)
