adb0320b52763b474250b66c2f123659c2d76846
Malan Himsara <87243006+redx414@users.noreply.github.com>
2021-07-24 23:43:29 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"<b>🔻Follow the Following Steps🔻/n/n◆Send a Youtube Video Url Directly to the Bot😉/n/n◈Pick the Quality that You Require😇/n/n◆Choose Wether You Want the Video as a Video or as a Document🙂/n/n◈It Will Take Some Time to Upload Your Video😐/n/n◆Now You Have Your Video😊/n/n◈Enjoy🙊❤️</b>"
+    helptxt = f"<b>🔻Follow the Following Steps🔻</b>\n\n◆Send a Youtube Video Url Directly to the Bot😉\n\n◈Pick the Quality that You Require😇\n\n◆Choose Wether You Want the Video as a Video or as a Document🙂\n\n◈It Will Take Some Time to Upload Your Video😐\n\n◆Now You Have Your Video😊\n\n<b>◈Enjoy🙊❤️</b>"
     await message.reply_text(helptxt)
